b399ba18017b0eb647441cbed8d32d2e3f25d890
Obed Paz <lesterpaz99@gmail.com>
2021-01-23 11:38:53 -0600

    I've created a function to bring data from localStorage to my front

--- app.js
+++ app.js
@@ -20,5 +20,26 @@ const saveTask = (event) => {
     }
     event.preventDefault();
 }
+
 //the submit event is only for entire forms, not for just a button.
 document.getElementById('formTask').addEventListener('submit', saveTask);
+
+const getTasks = () => {
+    let tasks = JSON.parse(localStorage.getItem('Tasks'));
+    let taskView = document.getElementById('task');
+
+    taskView.innerHTML = '';
+    for (element of tasks) {
+        let title = element.title;
+        let description = element.description;
+        taskView.innerHTML += `<div class="task-container">
+            <div>
+            <div><p>${title} - ${description}</p></div>
+            <div><button>Delete</button></div>
+            </div>
+        </div>
+        `
+    }
+}
+
+getTasks();
