50beb293b96c726372866dc228c7b886bed6a7ad
Obed Paz <lesterpaz99@gmail.com>
2021-01-27 23:17:26 -0600

    add an if that check if the values arent empty

--- app.js
+++ app.js
@@ -1,56 +1,61 @@
 const saveTask = (event) => {
-    let title = document.getElementById('title');
-    let description = document.getElementById('description');
+  let title = document.getElementById("title");
+  let description = document.getElementById("description");
 
+  if (
+    (title.value === "" && description.value === "") ||
+     title.value === "" || description.value === "") {
+    alert("Oops! Something went wrong. You have empty values.");
+  } else {
     const task = {
-        title: title.value, 
-        description: description.value 
+      title: title.value,
+      description: description.value,
     };
 
-    
-    if(localStorage.getItem('Tasks') === null) {
-        let tasksToDo = [];
-        tasksToDo.push(task);
-        localStorage.setItem('Tasks', JSON.stringify(tasksToDo));
+    if (localStorage.getItem("Tasks") === null) {
+      let tasksToDo = [];
+      tasksToDo.push(task);
+      localStorage.setItem("Tasks", JSON.stringify(tasksToDo));
     } else {
-        let tasksToDo = JSON.parse(localStorage.getItem('Tasks'));
-        tasksToDo.push(task);
-        localStorage.setItem('Tasks', JSON.stringify(tasksToDo));
+      let tasksToDo = JSON.parse(localStorage.getItem("Tasks"));
+      tasksToDo.push(task);
+      localStorage.setItem("Tasks", JSON.stringify(tasksToDo));
     }
+  }
 
-    document.getElementById('formTask').reset();
-    getTasks();
-    event.preventDefault();
-}
+  document.getElementById("formTask").reset();
+  getTasks();
+  event.preventDefault();
+};
 
 //the submit event is only for entire forms, not for just a button.
-document.getElementById('formTask').addEventListener('submit', saveTask);
+document.getElementById("formTask").addEventListener("submit", saveTask);
 
 const getTasks = () => {
-    let tasks = JSON.parse(localStorage.getItem('Tasks'));
-    let taskView = document.getElementById('task');
+  let tasks = JSON.parse(localStorage.getItem("Tasks"));
+  let taskView = document.getElementById("task");
 
-    taskView.innerHTML = '';
-    for (element of tasks) {
-        let title = element.title;
-        let description = element.description;
-        taskView.innerHTML += `<div class="task-container">
+  taskView.innerHTML = "";
+  for (element of tasks) {
+    let title = element.title;
+    let description = element.description;
+    taskView.innerHTML += `<div class="task-container">
             <div><p><span class="taskTitle">${title}</span> - ${description}</p></div>
             <div><button class="deleteTask" id="deleteTask" onClick="deleteTask('${title}')">Delete</button></div>
         </div>
-        `
-    }
-}
+        `;
+  }
+};
 
 getTasks();
 
 const deleteTask = (title) => {
-    let tasks = JSON.parse(localStorage.getItem('Tasks'));
-    for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].title == title) {
-            tasks.splice(i, 1);
-        }
+  let tasks = JSON.parse(localStorage.getItem("Tasks"));
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].title == title) {
+      tasks.splice(i, 1);
     }
-    localStorage.setItem('Tasks', JSON.stringify(tasks));
-    getTasks();
-}
+  }
+  localStorage.setItem("Tasks", JSON.stringify(tasks));
+  getTasks();
+};
